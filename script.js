163842f1ce19424c55c82134d7a76b2b5b61e469
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-04-03 22:15:20 -0400

    updating lego review page

--- script.js
+++
@@ -1,3 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Welcome to my website. I'm currently in the early stages of my coding journey, but I believe I can create a website that is decently interesting. Feel free to look around :).";
-document.querySelector("body").appendChild(h2);
